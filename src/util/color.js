c74526c2898268c466152bab10d128f152dc0c8c
James McClung <james@James-MacBook-Pro.local>
2018-06-01 11:33:31 -0400

    made pretty colors

--- src/util/color.js
+++ src/util/color.js
@@ -1,1 +1,11 @@
-export const levelColor = (level) => `rgb(0, ${level * 25}, 0)`;
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
+
+const LOW_R = 50, HIGH_R = 150,
+    LOW_G = 220, HIGH_G = 0,
+    LOW_B = 250, HIGH_B = 120;
+
+const r = (level) => LOW_R + (HIGH_R - LOW_R) / 10 * level;
+const g = (level) => LOW_G + (HIGH_G - LOW_G) / 10 * level;
+const b = (level) => LOW_B + (HIGH_B - LOW_B) / 10 * level;
+
+export const levelColor = (level) => `rgb(${r(level)}, ${g(level)}, ${b(level)})`;
